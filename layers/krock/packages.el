ff201ba2e611254c52ee89deaef8fcd94cb2a7c4
王栋 <wd@dorado.cc>
2017-10-16 18:31:25 +0800

    增加git-gutter

--- layers/krock/packages.el
+++ layers/krock/packages.el
@@ -32,10 +32,15 @@
 (defconst krock-packages
   '(
     ;;nodejs-repl
+    git-gutter
     ))
 (defun krock/init-nodejs-repl()
   (use-package nodejs-repl
     :ensure t)
+  (use-package git-gutter
+    :ensure t
+    :init
+    (global-git-gutter-mode +1))
   )
 (defun krock/post-init-nodejs-repl()
     (add-hook 'js-mode-hook
